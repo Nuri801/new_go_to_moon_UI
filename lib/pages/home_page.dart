00690b0f09283444190aac8748203ee9f8b6ac20
Nuri <nuriddin7612@mail.ru>
2022-05-27 15:45:03 +0900

    gotomoon_Image_imported

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,12 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/images/img.png'))
+        ),
+      ),
+    );
   }
 }
