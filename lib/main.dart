40916dceb829d2bf8cb2687cbc811e01acff3a85
Nuri <nuriddin7612@mail.ru>
2022-05-27 15:18:55 +0900

    gotomoon_UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
@@ -10,14 +10,20 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Go Moon',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            '#Go To Moon',
-            style: TextStyle(
-                fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-        ),
+        backgroundColor: Color.fromARGB(31, 31, 31, 1),
+        // body: Container(
+        //   child: Text('Go To Moon', style: TextStyle(color: Colors.white),),
+        // ),
+        // appBar: AppBar(
+        //   title: const Text(
+        //     '#Go To Moon',
+        //     style: TextStyle(
+        //         fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
+        //   ),
+        // ),
+        // body: Container(),
       ),
     );
   }
