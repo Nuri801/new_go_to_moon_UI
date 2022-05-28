f18a7ae9b442a3250eb8e01ec5e88ba54188aa5e
Nuri <nuriddin7612@mail.ru>
2022-05-28 17:31:09 +0900

    SafeArea

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pageTitle(),
+      body: SafeArea(
+        child: _pageTitle(),
+      ),
     );
   }
 
