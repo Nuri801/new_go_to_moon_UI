96b5e6a5f265bc5a909e76a5673313b433ca47ad
Nuri <nuriddin7612@mail.ru>
2022-05-30 13:20:07 +0900

    inserting Everything into a Column

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,6 +18,7 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               _pageTitle(),
               // _astroImageWidget(),
